0ba1635ce9194b999265a3fe26ecc2e635a27fef
Sanjuu516 <sanjananarni516@gmail.com>
2025-06-21 18:19:53 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Carbon-Calculator/',
-  plugins: [react()],
+  base: process.env.NODE_ENV === 'production' ? '/Carbon-Calculator/' : '/',
+  plugins: [react()]
 })
-
