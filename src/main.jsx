69b7108715568376f090b385c523da0391d7890a
Sanjuu516 <sanjananarni516@gmail.com>
2025-06-21 18:37:41 +0530

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";  // This includes Tailwind CSS
+import "./index.css";
+
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/Carbon-Calculator/">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/Carbon-Calculator/' : '/',
-  plugins: [react()]
+  base: '/Carbon-Calculator/',
+  plugins: [react()],
 })
